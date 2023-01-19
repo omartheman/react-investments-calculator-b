78645c7fa808d5419be85a71e8af3493c5e0068f
Omar Shishani <omarshishanischool@gmail.com>
2023-01-19 17:52:14 -0500

    Calculate annual interest

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,38 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-
+import { useState, useEffect } from 'react'
+ 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const [dollarsPerMonth, setDollarsPerMonth] = useState(0)
+  const [interestRate, setInterestRate] = useState(0)
+  const [yearsInvested, setYearsInvested] = useState(0)
+  const [lowerAge, setLowerAge] = useState(0)
+  const [upperAge, setUpperAge] = useState(0)
+  const [finalAmount, setFinalAmount] = useState(0)
+
+  useEffect(() => {
+    setYearsInvested(upperAge - lowerAge)
+
+    // Calculate final amount
+    setFinalAmount(() => (
+      yearsInvested * interestRate
+    ))
+  }, [upperAge, lowerAge, dollarsPerMonth, interestRate, yearsInvested])
+
+  const amountPerYear = []
+  for (let i = 0; i < 20; i++){
+    console.log('dollars', dollarsPerMonth, 'amount:', amountPerYear, 'interest', interestRate, 'i: ', i)
+
+    
+    amountPerYear.push(dollarsPerMonth * 12 + (amountPerYear.length > 0 && amountPerYear[i - 1] * (interestRate+1)) )
+    console.log(amountPerYear)
+  }
+
+
   return (
     <>
       <Head>
@@ -15,107 +43,64 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
+        <div>
+          <h1>Investment Calculator</h1>
           <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
+            <label>
+              Amount invested every month: 
+              <input  
+                onChange={(e) => {
+                  setDollarsPerMonth(e.target.value)
+                }}
               />
-            </a>
+            </label>
+            <div>{dollarsPerMonth}</div>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
+          <div>
+            <label>
+              Interest rate (%):
+              <input  
+                onChange={(e) => {
+                  setInterestRate(e.target.value / 100)
+                }}
+              />
+            </label>
+            <div>{interestRate}</div>
           </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+          <div>
+            <label>
+              Age range invested: from 
+              <input  
+                onChange={(e) => {
+                  setLowerAge(e.target.value)
+                  setYearsInvested(upperAge - lowerAge)
+                }}
+              />
+              to 
+              <input  
+                onChange={(e) => {
+                  setUpperAge(e.target.value)
+                }}
+              />
+            </label>
+            <div>{yearsInvested} years of investing</div>
+          </div>
+          <div>
+            Amount of money at end: {finalAmount}
+          </div>
+          <div>
+            Amount per year: {
+              amountPerYear.map((value, index) => {
+                return(
+                  <>
+                    <div>
+                      Year {index + 1}: {value},
+                    </div>
+                  </>
+                )
+              })
+            }
+          </div>
         </div>
       </main>
     </>
