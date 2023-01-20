cf1b7a0e9ae76f17a6a5425ddd4b145fc254c006
Omar Shishani <omarshishanischool@gmail.com>
2023-01-19 22:50:00 -0500

    Fix mobile padding

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main}`}>
+      <main className={`${styles.main} p-6`}>
         <div>
           <h1 className="text-center text-2xl">Investment Calculator</h1>
 
