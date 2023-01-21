56fff987effe0c6a6bca0deed2a8576914b45ce7
Omar Shishani <omarshishanischool@gmail.com>
2023-01-20 19:44:58 -0500

    Add title and styling

--- components/BarChart.js
+++ components/BarChart.js
@@ -1,41 +1,66 @@
 import { VictoryLabel, VictoryArea, VictoryAxis, VictoryTheme, VictoryChart, VictoryBar } from 'victory'
 
 export default function BarChart({lowerAge, finalAge, annualData, amountPerYear}){
+  
   return(
-    <VictoryChart domainPadding={15}>
-      <VictoryAxis 
-        dependentAxis={true} 
-        style={{ 
-          grid: {
-            stroke: "grey",
-          }
+    <>
+      <h2>Label</h2>
+      <VictoryChart 
+        domainPadding={{x: 15, y: 0}}
+        theme={{
+          axis: {
+            style: {
+              tickLabels: {
+                fill: 'white',
+              }
+            }
+          },
         }}
-        tickFormat={(t) => `${t / 1000}k`}
-      />
-      <VictoryAxis crossAxis
-        domain={[lowerAge, finalAge]}
-        theme={VictoryTheme.material}
-        standalone={false}
-      />
-      <VictoryBar 
-        style={{data: {fill: '#b1b1b1'}}}
-        data={annualData}
-      /> 
-      {
-        amountPerYear.map((data, index) => {
-          return(
-            <VictoryAxis 
-              dependentAxis={true} 
-              key={index}
-              label={`$${Math.floor(data).toLocaleString()}`}
-              style={{ 
-                tickLabels: { fill: "none" }
-              }}
-              axisValue={data}
-            />
-          )
-        })
-      }
-    </VictoryChart>
+      >  
+        <VictoryLabel text="Age v. Total Amount Saved"
+          x={225} y={40}
+          textAnchor="middle"
+          style={{
+            fill: 'white'
+          }}
+        />
+        <VictoryAxis dependentAxis
+          style={{ 
+            grid: {
+              stroke: "grey",
+            }
+          }}
+          tickFormat={(t) => `${t / 1000}k`}
+          />
+        <VictoryAxis crossAxis
+          domain={[lowerAge, finalAge]}
+          theme={VictoryTheme.material}
+          standalone={false}
+          label="Age (years)"
+          style={{fill: "white"}}
+        />
+        <VictoryBar 
+          style={{
+            data: {fill: '#b1b1b1'}
+          }}
+          data={annualData}
+        /> 
+        {
+          amountPerYear.map((data, index) => {
+            return(
+              <VictoryAxis 
+                dependentAxis={true} 
+                key={index}
+                label={`$${Math.floor(data).toLocaleString()}`}
+                style={{ 
+                  tickLabels: { fill: "none" }
+                }}
+                axisValue={data}
+              />
+            )
+          })
+        }
+      </VictoryChart>
+    </>
   )
 }
