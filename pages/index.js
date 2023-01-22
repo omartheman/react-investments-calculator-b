d56f6060136bfea72ac58429b2d77a50c08c57fb
Omar Shishani <omarshishanischool@gmail.com>
2023-01-21 21:31:44 -0500

    Remove test data

--- pages/index.js
+++ pages/index.js
@@ -59,13 +59,7 @@ export default function Home() {
   const taxRateDecimal = taxRatePercentage / 100; 
   const finalAmount = amountPerYear[amountPerYear.length - 1]; 
   const annualIncomeAfterTaxes = finalAmount*interestRateDecimal * (1 - taxRateDecimal); 
-  
-  const data = [
-    {quarter: 1, earnings: 13000},
-    {quarter: 2, earnings: 16500},
-    {quarter: 3, earnings: 14250},
-    {quarter: 4, earnings: 19000}
-  ];
+
 
   const annualData = [
   ]
