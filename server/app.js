b11355ef44b8e8f91e4ab08d651680e5e4826972
Omar Shishani <omarshishanischool@gmail.com>
2023-08-16 08:55:25 -0400

    Add express session code

--- server/app.js
+++ server/app.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const session = require('express-session')
 const app = express() 
 const port = 5001; 
+
+app.use(session({
+  secret: 'keyboard-cat',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    secure: false,
+  }
+}))
 
 app.get('/', function (req, res) {
   res.send('GET request to homepage')
@@ -8,4 +18,5 @@ app.get('/', function (req, res) {
 
 app.listen(port, () => {
   console.log('now listening on port ' + port )
+  console.log('session:', session)
 })
